548ee761287637971338f3e29db209619d768227
chamika04 <159267428+chamika04@users.noreply.github.com>
2026-01-03 11:39:14 +0530

    Update 2

---
+++ LAtes/src/App.jsx
@@ -0,0 +1,31 @@
+import './App.css'
+import { Outlet } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import MyFooter from './components/MyFooter'
+import { useState } from 'react'
+
+function App() {
+  const [show, setShow] = useState(true)
+  const [cart, setCart] = useState([])
+
+  const handleClick = (product) => {
+    let isPresent = false
+    cart.forEach((item) => {
+      if (product._id === item._id) isPresent = true
+    })
+    if (isPresent) return
+    setCart([...cart, product])
+  }
+
+  return (
+    <>
+      <Navbar />
+      <div className="min-h-screen">
+        <Outlet /> {/* React Router will render child routes here */}
+      </div>
+      <MyFooter />
+    </>
+  )
+}
+
+export default App
