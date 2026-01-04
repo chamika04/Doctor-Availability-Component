0702f3a44b1ec5d6ab8a97066f684f98787f8cb1
chamika04 <159267428+chamika04@users.noreply.github.com>
2026-01-04 17:02:56 +0530

    Update router.jsx

--- LAtes/src/routers/router.jsx
+++ LAtes/src/routers/router.jsx
@@ -1,5 +1,6 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import axios from "axios";
+
 import App from "../App";
 import Home from "../home/Home";
 import Shop from "../shop/Shop";
@@ -16,12 +17,12 @@ import UploadProduct from "../dashboard/UploadProduct";
 import EditProduct from "../dashboard/EditProduct";
 import ManageProduct from "../dashboard/ManageProduct";
 
-
 import A_ContactUs from "../Review/Admin_Side/A_ContactUs";
 import A_ReviewBoxes from "../Review/Admin_Side/A_ReviewBoxes";
 import A_Instructor from "../Review/Admin_Side/A_Instructor";
 import A_product from "../Review/Admin_Side/A_product";
 import A_workout from "../Review/Admin_Side/A_workout";
+
 import ContactUs from "../Review/User_Side/ContactUs";
 import F_Instructor from "../Review/User_Side/F_Instructor";
 import F_product from "../Review/User_Side/F_product";
@@ -41,9 +42,9 @@ import ReviewDistribution from "../Review/Admin_Side/ReviewDistribution";
 
 import Card from "../Payment_Management/card";
 import Choose_payment_method from "../Payment_Management/choose_payment_method";
-import Received from "../Payment_Management/received"
-import Salary_cal from "../Payment_Management/salary_cal"
-import Slp from "../Payment_Management/slp"
+import Received from "../Payment_Management/received";
+import Salary_cal from "../Payment_Management/salary_cal";
+import Slp from "../Payment_Management/slp";
 import ViewCards from "../Payment_Management/viewww";
 import Paysucc from "../Payment_Management/paysucc";
 
@@ -80,312 +81,109 @@ import MealPlan from "../components/nutrition/MealPlan";
 import MealDashboard from "../components/nutrition/MealDashboard";
 import UserForm from "../components/nutrition/UserForm";
 import NutrionistBooking from "../components/nutrition/NutrionistBooking";
+
 import DoctorProfile from "../pages/DoctorProfile";
 import Appointments from "../pages/Appointments";
 import DoctorManageAdmin from "../pages/DoctorManageAdmin";
 import UserDashboard from "../pages/UserDashboard";
+import ETUAdminDashboard from "../pages/ETUAdminDashboard";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
     children: [
+      { path: "/", element: <Home /> },
+      { path: "/admin/dashboard/etu", element: <ETUAdminDashboard /> },
+      { path: "/user-dashboard", element: <UserDashboard /> },
+      { path: "/doctormangeadmin", element: <DoctorManageAdmin /> },
+      { path: "/appointments", element: <Appointments /> },
+      { path: "/doctor/:id", element: <DoctorProfile /> },
+      { path: "/doctor-availability", element: <DoctorAvailability /> },
+      { path: "/shop", element: <Shop /> },
+      { path: "/productHome", element: <ProductHome /> },
+      { path: "/workoutHome", element: <WorkoutHome /> },
+      { path: "/bookingHome", element: <BookingHome /> },
+      { path: "/reviewHome", element: <ReviewHome /> },
+      { path: "/about", element: <About /> },
+      { path: "/ViewSchedule", element: <ScheduleView /> },
+      { path: "/MakeBooking", element: <MakeBooking /> },
+      { path: "/BookingDetails", element: <BookingDetails /> },
+      { path: "/contactUs", element: <ContactUs /> },
+      { path: "/f_Instructor", element: <F_Instructor /> },
+      { path: "/f_product", element: <F_product /> },
+      { path: "/f_workout", element: <F_workout /> },
+      { path: "/Review_instructor", element: <Instructor /> },
+      { path: "/Review_workout", element: <Workout /> },
+      { path: "/Review_products", element: <Product /> },
+      { path: "/reviewBoxes", element: <ReviewBoxes /> },
+      { path: "/show_I", element: <Show_I /> },
+      { path: "/show_P", element: <Show_P /> },
+      { path: "/show_W", element: <Show_W /> },
+      { path: "/instructorRating", element: <InstructorRating /> },
+      { path: "/workoutRating", element: <WorkoutRating /> },
+      { path: "/productRating", element: <ProductRating /> },
+      { path: "/showP4Shop", element: <ShowP4Shop /> },
+      { path: "/card", element: <Card /> },
+      { path: "/paysucc", element: <Paysucc /> },
+      { path: "/choose_payment_method", element: <Choose_payment_method /> },
+      { path: "/slp", element: <Slp /> },
+      { path: "/view_cards", element: <ViewCards /> },
+      { path: "/login", element: <Username /> },
+      { path: "/password", element: <Password /> },
+      { path: "/register", element: <Register /> },
+      { path: "/bmi", element: <BMI /> },
+      { path: "/profile", element: <Profile /> },
+      { path: "/recovery", element: <Recovery /> },
+      { path: "/reset", element: <Reset /> },
       {
-        path: "/",
-        element: <Home />,
+        path: "/product/:id",
+        element: <SingleProduct />,
+        loader: ({ params }) =>
+          fetch(`http://localhost:5000/product/${params.id}`),
       },
-      {
-        path: "/user-dashboard",
-        element: <UserDashboard />,
-      },
-      {
-        path: "/doctormangeadmin",
-        element: <DoctorManageAdmin />,
-      },
-      {
-        path: "/appointments",
-        element: <Appointments />,
-      },
-      {
-        path: "/doctor/:id",
-        element: <DoctorProfile />
-      },
-      {
-      path:"/doctor-availability",
-      element: <DoctorAvailability />
-      },
-      {
-        path: "/shop",
-        element: <Shop/>,
-      },
-      {
-        path: "/productHome",
-        element: <ProductHome/>
-      },
-      {
-        path: "/workoutHome",
-        element: <WorkoutHome/>
-      },
-      {
-        path: "/bookingHome",
-        element: <BookingHome/>
-      },
-      {
-        path: "/reviewHome",
-        element: <ReviewHome/>
-      },
-      {
-        path: "/about",
-        element: <About/>
-      },
-      {
-        path:"/ViewSchedule",
-        element:<ScheduleView />
-      },
-      {
-        path:"/MakeBooking",
-        element:<MakeBooking />
-      }, {
-        path:"/BookingDetails",
-        element:<BookingDetails />
-      },
-      {
-        path: "/contactUs",
-        element: <ContactUs />,
-      },
-      {
-        path: "/f_Instructor",
-        element: <F_Instructor />,
-      },
-      {
-        path: "/f_product",
-        element: <F_product />,
-      },
-      {
-        path: "/f_workout",
-        element: <F_workout />,
-      },
-      {
-        path: "/Review_instructor",
-        element: <Instructor />,
-      },
-      {
-        path: "/Review_workout",
-        element: <Workout />,
-      },
-      {
-        path: "/Review_products",
-        element: <Product />,
-      },
-      {
-        path: "/reviewBoxes",
-        element: <ReviewBoxes />,
-      },
-      {
-        path: "/show_I",
-        element: <Show_I />,
-      },
-      {
-        path: "/show_P",
-        element: <Show_P />,
-      },
-      {
-        path: "/show_W",
-        element: <Show_W />,
-      },
-      {
-        path: "/instructorRating",
-        element: <InstructorRating />, 
-      },
-      {
-        path: "/workoutRating",
-        element: <WorkoutRating />, 
-      },
-      {
-        path: "/productRating",
-        element: <ProductRating />, 
-      },
-      {
-        path: "/showP4Shop",
-        element: <ShowP4Shop />, 
-      },
-      {
-        path: "/card",
-        element: <Card/>,
-      },
-      {
-        path: "/paysucc",
-        element: <Paysucc/>,
-      },
-      {
-        path: "/choose_payment_method",
-        element: <Choose_payment_method />,
-      },
-      {
-        path: "/slp",
-        element: <Slp/>,
-      },
-      {
-         path: "/view_cards",
-       element: <ViewCards/>,
-       }, {
-        path: "/login",
-        element: <Username />,
-      },
-      {
-          path : '/password',
-          element : <Password/>
-      },
-      {
-          path : '/register',
-          element : <Register></Register>
-       },
-      {
-          path : '/bmi',
-          element : <BMI></BMI>
-      },
-      {
-          path : '/profile',
-          element : <Profile></Profile>
-      },
-      {
-          path : '/recovery',
-          element : <Recovery></Recovery>
-      },
-      {
-          path : '/reset',
-          element : <Reset></Reset>
-      },
-      {
-        path:"/product/:id",
-        element:<SingleProduct/>,
-        loader:({params})=>fetch(`http://localhost:5000/product/${params.id}`)
-      },
-      {
-        path: "/WorkoutUserView",
-        element: <WorkoutUserView/>,
-      },
-      {
-        path: "/MyWorkout",
-        element: <MyWorkout/>,
-      }
-      , {
-        path:"/MyMeals",
-        element:<MyMeals />
-      }
-      , {
-        path:"/MealHome",
-        element:<MealHome />
-      },
-      {
-        path: "/MealPlan",
-        element: <MealPlan/>,
-      },
-      {
-        path: "/MealDashboard",
-        element: <MealDashboard/>,
-      }
-      , {
-        path:"/UserForm",
-        element:<UserForm />
-      }
-      , {
-        path:"/NutrionistBooking",
-        element:<NutrionistBooking />
-      }
+      { path: "/WorkoutUserView", element: <WorkoutUserView /> },
+      { path: "/MyWorkout", element: <MyWorkout /> },
+      { path: "/MyMeals", element: <MyMeals /> },
+      { path: "/MealHome", element: <MealHome /> },
+      { path: "/MealPlan", element: <MealPlan /> },
+      { path: "/MealDashboard", element: <MealDashboard /> },
+      { path: "/UserForm", element: <UserForm /> },
+      { path: "/NutrionistBooking", element: <NutrionistBooking /> },
     ],
   },
-  
-
-  
-  
-  
   {
-    path:"/admin/dashboard",
-    element:<DashboardLayout/>,
-    children:[
+    path: "/admin/dashboard",
+    element: <DashboardLayout />,
+    children: [
+      { path: "/admin/dashboard", element: <Dashboard /> },
+      { path: "/admin/dashboard/user", element: <Admin /> },
+      { path: "/admin/dashboard/upload", element: <UploadProduct /> },
+      { path: "/admin/dashboard/manage", element: <ManageProduct /> },
       {
-        path:"/admin/dashboard",
-        element:<Dashboard/>
-      }, 
-      {
-        path : '/admin/dashboard/user',
-        element : <Admin></Admin>
-      },
-      {
-        path:"/admin/dashboard/upload",
-        element:<UploadProduct/>
-      },
-      {
-        path:"/admin/dashboard/manage",
-        element:<ManageProduct/>
-      },
-      {
-        path:"/admin/dashboard/edit/:id",
-        element:<EditProduct/>,
+        path: "/admin/dashboard/edit/:id",
+        element: <EditProduct />,
         loader: ({ params }) =>
-        axios.get(`http://localhost:8070/product/get/${params.id}`)
-        .then(response => response.data)
+          axios
+            .get(`http://localhost:8070/product/get/${params.id}`)
+            .then((response) => response.data),
       },
-      {
-        path: "/admin/dashboard/a_ReviewBoxes",
-        element: <A_ReviewBoxes />,
-      },
-      {
-        path: "/admin/dashboard/a_ContactUs",
-        element: <A_ContactUs />,
-      },
-      {
-        path: "/admin/dashboard/a_Instructor",
-        element: <A_Instructor />,
-      },
-      {
-        path: "/admin/dashboard/a_product",
-        element: <A_product />,
-      },
-      {
-        path: "/admin/dashboard/a_workout",
-        element: <A_workout />,
-      },
-      {
-        path: "/admin/dashboard/distribution",
-        element: <ReviewDistribution />,
-      },
-      {
-        path: "/admin/dashboard/received",
-        element: <Received />,
-      },
-      {
-        path: "/admin/dashboard/salary_cal",
-        element: <Salary_cal/>,
-      }, {
-        path:"/admin/dashboard/BookingRequest",
-        element:<BookingRequest />
-      }, {
-        path:"/admin/dashboard/ManagerScheduleView",
-        element:<ManagerScheduleView />
-      }, {
-        path:"/admin/dashboard/ExerciseForm",
-        element:<ExerciseForm />
-      }
-      , {
-        path:"/admin/dashboard/ExerciseList",
-        element:<ExerciseList />
-      }
-      , {
-        path:"/admin/dashboard/WorkoutForm",
-        element:<WorkoutForm />
-      }
-      , {
-        path:"/admin/dashboard/WorkoutList",
-        element:<WorkoutList />
-      }
-      
-      
-      
-    ]
-  }
- 
+      { path: "/admin/dashboard/a_ReviewBoxes", element: <A_ReviewBoxes /> },
+      { path: "/admin/dashboard/a_ContactUs", element: <A_ContactUs /> },
+      { path: "/admin/dashboard/a_Instructor", element: <A_Instructor /> },
+      { path: "/admin/dashboard/a_product", element: <A_product /> },
+      { path: "/admin/dashboard/a_workout", element: <A_workout /> },
+      { path: "/admin/dashboard/distribution", element: <ReviewDistribution /> },
+      { path: "/admin/dashboard/received", element: <Received /> },
+      { path: "/admin/dashboard/salary_cal", element: <Salary_cal /> },
+      { path: "/admin/dashboard/BookingRequest", element: <BookingRequest /> },
+      { path: "/admin/dashboard/ManagerScheduleView", element: <ManagerScheduleView /> },
+      { path: "/admin/dashboard/ExerciseForm", element: <ExerciseForm /> },
+      { path: "/admin/dashboard/ExerciseList", element: <ExerciseList /> },
+      { path: "/admin/dashboard/WorkoutForm", element: <WorkoutForm /> },
+      { path: "/admin/dashboard/WorkoutList", element: <WorkoutList /> },
+    ],
+  },
 ]);
 
 export default router;
